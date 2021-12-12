b53e97d78a0b34c93a501ce0e12eee9a36e9c724
ganudoomer <ganuiscool@gmail.com>
2021-12-12 18:54:20 +0530

    Initial Commit

---
+++ BucketListv1/BucketListv1App.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListv1App.swift
+//  BucketListv1
+//
+//  Created by Sree on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
